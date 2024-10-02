e02a056c370220a9f7c0622233d8d37769203cdb
john25552 <139536361+john25552@users.noreply.github.com>
2024-10-02 07:35:11 -1200

    Update api.ts

--- src/stores/api.ts
+++ src/stores/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create a new instance of Axios with custom configurations
 const api = axios.create({
-  baseURL: 'https://notesphere-sys-production.up.railway.app/',
+  baseURL: 'https://notesanchor-production.up.railway.app/',
   // baseURL: 'http://localhost:3000/',
   // timeout: 10000,
   withCredentials: true
